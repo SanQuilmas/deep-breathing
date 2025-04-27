0e1010889a72efec7d9dde61ed7e30124fcc5d07
Leonel Gonzalez <leonel.gvalencia@alumnos.udg.mx>
2025-04-27 11:52:49 -0600

    Slight changes to styled components

--- src/components/Circle/Circle.styles.ts
+++ src/components/Circle/Circle.styles.ts
@@ -3,13 +3,16 @@ import styled from "styled-components";
 export const CircleDiv = styled.div`
     width: fit-content;
     aspect-ratio: 1 / 1;
-    padding: 0.5rem;
+
     border-radius: 50%;
     border: 1px solid red;
+
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    padding: 0.5rem;
+    transition: all 0.3s ease-in-out;
 
     &:hover {
         background-color: rgba(255, 0, 0, 0.1);
