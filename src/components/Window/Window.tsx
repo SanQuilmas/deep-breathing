d3fb62a50424a9bc2209d854fd573a7aac473445
lgonzalezvalencia <lgonzalezvalencia@deloitte.com>
2025-04-28 15:12:14 -0600

    Add context

--- src/components/Window/Window.tsx
+++ src/components/Window/Window.tsx
@@ -3,18 +3,28 @@ import { BPMInput } from "../BPMInput/BPMInput"
 import { TimeSelector } from "../TimeSelector/TimeSelector"
 import { Header, MainArea, WindowContainer } from "./Window.styles"
 import { ThemeToggle } from "../ThemeToggle/ThemeToggle"
+import { createContext } from "react"
+
+interface MainContextProps {
+    bpm: number
+    theme: string
+}
+
+const MainContext = createContext({} as MainContextProps)
 
 export const Window = () =>{
     return (
-        <WindowContainer>
-            <Header>
-                <BPMInput />
-                <ThemeToggle />
-            </Header>
-            <MainArea>
-                <TimeSelector />
-                <Circle/>
-            </MainArea>
-        </WindowContainer>
+        <MainContext.Provider value={{} as MainContextProps}>
+            <WindowContainer>
+                <Header>
+                    <BPMInput />
+                    <ThemeToggle />
+                </Header>
+                <MainArea>
+                    <TimeSelector />
+                    <Circle/>
+                </MainArea>
+            </WindowContainer>
+        </MainContext.Provider>
     )
 }
