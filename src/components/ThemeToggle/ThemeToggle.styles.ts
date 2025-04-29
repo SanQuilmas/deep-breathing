4dd80a9cc224566affc9a1a462d2d69af2b1084e
Gomez Figueroa <cgomezfigueroa@deloitte.com>
2025-04-29 14:57:26 -0600

    Merge latest features

---
+++ src/components/ThemeToggle/ThemeToggle.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Slider = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background-color: #FF8967;
+`;
